d9b247da14360f7989638645e5675ab620af3d96
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2026-05-09 12:04:35 -0400

    refactor: Remove obsolete code for connecting HTML files
    Remove obsolete code for connecting "app.html" and "product-list.html" files

--- shopping-app/src/app/components/product-list/product-list.ts
+++ shopping-app/src/app/components/product-list/product-list.ts
@@ -2,11 +2,6 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-list',
-
-  // MUST USE to Define -> templateUrl: './app.html'
-  // PREVENTS "[ERROR] NG8001: 'app-product-list' is not a known element"
-  standalone: true,
-  
   imports: [],
   templateUrl: './product-list.html',
   styleUrl: './product-list.component.css',
