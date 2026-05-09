2f8aa6da7476be8109f9a606ea4da3af94612153
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2026-05-09 11:43:52 -0400

    feat: Connect "app.html" and "product-list.html"
    Connect "app.html" and "product-list.html" files to change default Angular homepage into shopping homepage

--- shopping-app/src/app/app.ts
+++ shopping-app/src/app/app.ts
@@ -1,9 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// CANNOT import "routes" from "./app.routes"
+// Importing "routes" Causes "[ERROR] NG1010: 'imports' must be an array of components, directives, pipes, or NgModules."
+import { Cart } from "./components/cart/cart";
+import { Confirmation } from "./components/confirmation/confirmation";
+import { ProductItem } from "./components/product-item/product-item";
+import { ProductItemDetail } from "./components/product-item-detail/product-item-detail";
+import { ProductList } from './components/product-list/product-list';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+
+  // MUST Import ALL Components to Define -> templateUrl: './app.html'
+  // PREVENTS "[ERROR] NG8001: 'app-product-list' is not a known element"
+  imports: [RouterOutlet, ProductList],
+  
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- shopping-app/src/app/components/product-list/product-list.ts
+++ shopping-app/src/app/components/product-list/product-list.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-list',
+
+  // MUST USE to Define -> templateUrl: './app.html'
+  // PREVENTS "[ERROR] NG8001: 'app-product-list' is not a known element"
+  standalone: true,
+  
   imports: [],
   templateUrl: './product-list.html',
   styleUrl: './product-list.component.css',
 })
-export class ProductList {}
+export class ProductList implements OnInit {
+  ngOnInit(): void {
+      //
+  }
+}
