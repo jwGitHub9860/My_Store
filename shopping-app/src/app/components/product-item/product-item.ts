bfb87a1961f8b4669b930546d1185c2ed0d14fbe
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2026-06-02 14:39:59 -0400

    feat: Find item purchase amounts greater than zero
    Create function that finds item purchase amounts greater than zero to find items being purchased

--- shopping-app/src/app/components/product-item/product-item.ts
+++ shopping-app/src/app/components/product-item/product-item.ts
@@ -1,21 +1,24 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormsModule } from "@angular/forms";
 import { Item } from "../../models/Item";
-import { ProductList } from "../../components/product-list/product-list";
+import { ProductItemService } from "../../services/product-item/product-item";
+import { ProductListService } from "../../services/product-list/product-list";
 
 @Component({
   selector: 'app-product-item',
-  imports: [FormsModule, ProductList],
+  imports: [FormsModule],
   templateUrl: './product-item.html',
   styleUrl: './product-item.component.css',
 })
 export class ProductItem implements OnInit {
   // TEMP: use to Display ALL ITEMS BEING PURCHASED?
+  allItems: Item[] = [];
+  
+  // Child Component of "cart" Parent Component
+  itemAmountList: number[] = [];
 
-  // TEMP: Change into "purchaseItemList" to Hold All Items being Purchased
-  // TEMP: Keep Array Here or Elsewhere?
-  // Parent Component of "product-list", "cart", and "product-item-detail" Child Components
-  chosenItem: Item[] = [];
+  // Holds All Items being Purchased
+  purchaseItemList: Item[] = [];
 
   // TEMP: Do NOT NEED this "@Output"
   // Sends Data to "cart" Component
@@ -24,27 +27,36 @@ export class ProductItem implements OnInit {
   // TEMP: Use to Send "purchaseItemList" to "cart" Parent Component with "product-item" AS Child Component
   // Sends Data to "product-item-detail" Component
   @Output() itemDescription: EventEmitter<Item> = new EventEmitter();
-  
-  allItems: Item[] = [];
 
-  constructor() {}
+  constructor(private productItemService: ProductItemService, private productListService: ProductListService) {}
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.allItems = this.productListService.getItemList();
+    this.itemAmountList = this.productItemService.getItemPurchaseAmountList();
 
+    // Pushes Items being Purchased into EMPTY "purchaseItemList" Array
+    for (let index = 0; index < this.itemAmountList.length; index++) {
+      // TEMP: use "!==" OR "!="?
+      if (this.itemAmountList[index] !== 0) {
+        this.purchaseItemList.push(this.allItems[index + 1]);
+      }
+    }
+  }
+
+  // TEMP: use to Send "purchaseItemList" to "cart" Webpage
+  // TEMP: Rename "submitChosenItem" to "submitPurchaseItemList"
   // TEMP: use for "parent-child" Component Relationship WITH "cart" Component
   // Obtains Chosen Item & Sends it to "cart" & "item-details" Webpages
   submitChosenItem(chosenItem: Item): void {
+    // TEMP: Use to Reset "itemAmountList"
     // Ensures "chosenItem" is Empty BEFORE Inputting New Chosen Item
-    this.chosenItem.pop();
+    //this.chosenItem.pop();
     
+    // TEMP: Do NOT Need this
     // CANNOT use "=" to Add "chosenItem"
-    this.chosenItem.push(chosenItem);
+    //this.chosenItem.push(chosenItem);
     
-    // TEMP: Rename "itemPurchaseAmount" to "chosenItemDetails" LATER
+    // TEMP: Rename "itemPurchaseAmount" to "chosenPurchaseItems" LATER
     //this.itemPurchaseAmount.emit(chosenItem);
   }
-  
-  // TEMP: Reset "itemAmountList" HERE?
-  // TEMP: use "<button>" Tag to Reset Item Purchase Amounts?
-  // TEMP: use "Submit" Feature?
 }
