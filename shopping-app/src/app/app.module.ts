6a76cc35b90539f2d9b76a5306b12262ab051eb2
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2026-06-07 21:20:06 -0400

    fix: Resolve incorrect setup of app routing module
    Resolve incorrect setup of app routing module to fix connection between root module and components

--- shopping-app/src/app/app.module.ts
+++ shopping-app/src/app/app.module.ts
@@ -1,29 +1,36 @@
-// TEMP: keep or Remove this File?
-// TEMP: Because it is Redundant
-import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+
+// TEMP: keep or Remove?
 import { FormsModule } from "@angular/forms";
 
 // Gains Access to HTTP Client
 import { HttpClientModule } from "@angular/common/http";
 
+// TEMP: keep or Remove?
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app";
 
-// CANNOT Import "App" Component Because it's Standalone & Cannot be Declared in "NgModule"
 @NgModule({
-  // CANNOT INPUT Components into "declarations" Because they're standalones & CANNOT be Declared in "@NgModule"
+  // Links All Components to App Module
+  declarations: [],
+  
+  // CANNOT INPUT "App" Component into "declarations" Because it's Standalone & CANNOT be Declared in "@NgModule"
   // Causes "[ERROR] NG6008: Component App is standalone, and cannot be declared in an NgModule"
   // Causes "[ERROR] NG6002: 'HttpClient' does not appear to be an NgModule class"
-  declarations: [],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AppComponent,
     HttpClientModule,
     NgbModule,
     FormsModule
   ],
-  providers: []
+  providers: [],
+
+  // Starting Component
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- shopping-app/src/app/app.ts
+++ shopping-app/src/app/app.ts
@@ -1,17 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-// CANNOT import "routes" from "./app.routes"
-// Importing "routes" Causes "[ERROR] NG1010: 'imports' must be an array of components, directives, pipes, or NgModules."
-import { Cart } from "./components/cart/cart";
-import { Confirmation } from "./components/confirmation/confirmation";
 import { NavBar } from "./components/nav-bar/nav-bar";
-import { ProductItem } from "./components/product-item/product-item";
-import { ProductItemDetail } from "./components/product-item-detail/product-item-detail";
-import { ProductList } from './components/product-list/product-list';
-
-// MUST IMPORT "services" to Create Links for "Product List" & "Cart" Text
-import { ProductListService } from "./services/product-list/product-list";
 
 @Component({
   selector: 'app-root',
@@ -20,19 +10,11 @@ import { ProductListService } from "./services/product-list/product-list";
   // PREVENTS "[ERROR] NG8001: 'app-product-list' is not a known element"
   imports: [
     RouterOutlet,
-    Cart,
-    Confirmation,
-    NavBar,
-    ProductItem,
-    ProductItemDetail,
-    ProductList
+    NavBar
   ],
-  
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class AppComponent {
   protected readonly title = signal('shopping-app');
-
-  constructor(private productListService: ProductListService) { }
 }
