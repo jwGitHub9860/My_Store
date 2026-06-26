90cfddb082e2103f0a083cac767904739affa7a8
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2026-06-26 17:21:24 -0400

    feat: Send total purchase cost to "confirmation" webpage
    Send total purchase cost to "confirmation" component to display total purchase cost on "confirmation" webpage after clicking on "Submit" checkout button on "cart" webpage

--- shopping-app/src/app/components/confirmation/confirmation.ts
+++ shopping-app/src/app/components/confirmation/confirmation.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BillingInformationService } from "../../services/billing-information/billing-information";
+import { ProductItemService } from "../../services/product-item/product-item";
 
 @Component({
   selector: 'app-confirmation',
@@ -9,14 +10,15 @@ import { BillingInformationService } from "../../services/billing-information/bi
 })
 export class Confirmation implements OnInit {
   customerName: string;
+  totalPurchaseCost: number;
   
-  constructor(private billingInformationService: BillingInformationService) {
+  constructor(private billingInformationService: BillingInformationService, private productItemService: ProductItemService) {
     this.customerName = '';
+    this.totalPurchaseCost = 0;
   }
 
   ngOnInit(): void {
     this.customerName = this.billingInformationService.getCustomerName();
-
-    // TEMP: get "total_purchase_cost" HERE
+    this.totalPurchaseCost = this.productItemService.getTotalPurchaseCost();
   }
 }
