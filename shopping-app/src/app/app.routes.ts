9ff113c5f4b06a78118c68af0e1aa8a96864b01b
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2026-06-29 14:34:58 -0400

    refactor: Remove obsolete route path
    Remove obsolete "product-item" route path

--- shopping-app/src/app/app.routes.ts
+++ shopping-app/src/app/app.routes.ts
@@ -1,10 +1,6 @@
 import { Routes } from '@angular/router';
 import { Cart } from "./components/cart/cart";
 import { Confirmation } from "./components/confirmation/confirmation";
-
-// TEMP: keep or remove "product-item"?
-import { ProductItem } from "./components/product-item/product-item";
-
 import { ProductItemDetail } from "./components/product-item-detail/product-item-detail";
 import { ProductList } from './components/product-list/product-list';
 
@@ -12,9 +8,6 @@ import { ProductList } from './components/product-list/product-list';
 export const routes: Routes = [
     { path: 'cart', component: Cart },
     { path: 'confirmation', component: Confirmation },
-
-    // TEMP: change "path" values?
-    { path: 'item', component: ProductItem },
     { path: 'item-details', component: ProductItemDetail },
 
     // Homepage
