3af2fce1d496b59b201f99cfe46a80cf8d527936
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2026-06-29 14:04:27 -0400

    feat: Add outline around item images
    Add outline around item images to emphasize and separate images

--- shopping-app/src/app/components/product-item/product-item.ts
+++ shopping-app/src/app/components/product-item/product-item.ts
@@ -1,22 +1,61 @@
 // "NgZone" - Prevents Angular Applications from being Zoneless
 // "NgZone" - Performs Change Detection for "Observable"
-import { Component, OnInit, NgZone } from '@angular/core';
-
-// TEMP: do I Still Need these Import Statements?
+import { Component, OnInit, Input, inject, NgZone } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { Router, ActivatedRoute } from "@angular/router";
+import { Item } from '../../models/Item';
 import { ProductItemService } from "../../services/product-item/product-item";
-import { ProductListService } from "../../services/product-list/product-list";
 
 @Component({
   selector: 'app-product-item',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './product-item.html',
   styleUrl: './product-item.component.css',
 })
 export class ProductItem implements OnInit {
-  constructor(private productItemService: ProductItemService, private productListService: ProductListService, private ngZone: NgZone) {
+  // Ensures "itemAmountList" is Updated once "Add to cart" Button is Pressed
+  // Ensures "itemAmountList" is SEPARATE from "itemPurchaseAmountList" Array in "product-item" Service File
+  itemAmountList: number[];
+  
+  // Values for Parent-Child Relationship between "product-list" & "product-item" Components
+  @Input() item: Item;
+  
+  // ONLY WAY to Move from "product-list" Webpage to "product-item-detail" Webpage WITHOUT RESETTING Chosen Item ID
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+  
+  constructor(private productItemService: ProductItemService, private ngZone: NgZone) {
     // Checks if Application is Currently Running in Zoneless Mode
     console.log('Constructor zone:', this.ngZone.constructor.name);
+
+    this.itemAmountList = [ 0, 0, 0, 0, 0, 0 ];
+    this.item = {
+      id: 0,
+      name: '',
+      price: 0,
+      url: '',
+      description: ''
+    }
   }
 
-  ngOnInit() { }
+  ngOnInit(): void { }
+
+  // Submits Chosen Item to "product-item" Service File AND Moves to "product-item-detail" Webpage
+  submitChosenItem(id: number): void {
+    // Stores Chosen Item ID in "product-item" Service File
+    this.productItemService.setChosenItemId(id);
+
+    // Navigates to Item Details Webpage
+    // Ensures "item-details" Webpage Opens WHEN Item Image is Clicked On
+    // ONLY WAY to Move from "product-list" Webpage to "product-item-detail" Webpage WITHOUT RESETTING Chosen Item ID
+    this.router.navigate(['item-details'], {relativeTo: this.route});
+  }
+
+  addItemsToCart(id: number, price: number): void {
+    this.productItemService.setItemPurchaseAmount(this.itemAmountList[id - 1], id, price);
+
+    // Obtains item Price for Calculating Total Purchase Cost
+    this.productItemService.setTotalPurchaseCost(this.itemAmountList[id - 1], price);
+  }
 }
--- shopping-app/src/app/components/product-list/product-list.ts
+++ shopping-app/src/app/components/product-list/product-list.ts
@@ -1,61 +1,31 @@
 // "NgZone" - Prevents Angular Applications from being Zoneless
 // "NgZone" - Performs Change Detection for "Observable"
-import { Component, OnInit, inject, NgZone } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { CommonModule } from "@angular/common";
-import { FormsModule } from "@angular/forms";
-import { Router, ActivatedRoute } from "@angular/router";
 import { Observable } from 'rxjs';
 import { Item } from '../../models/Item';
-import { ProductItemService } from "../../services/product-item/product-item";
+import { ProductItem } from "../../components/product-item/product-item";
 import { ProductListService } from "../../services/product-list/product-list";
 
 @Component({
   selector: 'app-product-list',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, ProductItem],
   templateUrl: './product-list.html',
   styleUrl: './product-list.component.css',
 })
 export class ProductList implements OnInit {
   // Receives Item List from "Observable"
   items$!: Observable<Item[]>;
-
-  // Ensures "itemAmountList" is Updated once "Add to cart" Button is Pressed
-  // Ensures "itemAmountList" is SEPARATE from "itemPurchaseAmountList" Array in "product-item" Service File
-  itemAmountList: number[];
-
-  // ONLY WAY to Move from "product-list" Webpage to "product-item-detail" Webpage WITHOUT RESETTING Chosen Item ID
-  private route = inject(ActivatedRoute);
-  private router = inject(Router);
-
-  constructor(private productItemService: ProductItemService, private productListService: ProductListService, private ngZone: NgZone) {
+  
+  constructor(private productListService: ProductListService, private ngZone: NgZone) {
     // Checks if Application is Currently Running in Zoneless Mode
     console.log('Constructor zone:', this.ngZone.constructor.name);
-
-    this.itemAmountList = [ 0, 0, 0, 0, 0, 0 ];
   }
   
-  ngOnInit() {
+  ngOnInit(): void {
     // Uses "AsyncPipe" to Refresh User Interface
     // "AsyncPipe" -> Receives New Value from "Observable"
     // Do NOT NEED "subscribe" Call
     this.items$ = this.productListService.getItemList();
-  }
-
-  // Submits Chosen Item to "product-item" Component
-  submitChosenItem(id: number): void {
-    // Stores Chosen Item ID in "product-item" Service File
-    this.productItemService.setChosenItemId(id);
-
-    // Navigates to Item Details Webpage
-    // Ensures "item-details" Webpage Opens WHEN Item Image is Clicked On
-    // ONLY WAY to Move from "product-list" Webpage to "product-item-detail" Webpage WITHOUT RESETTING Chosen Item ID
-    this.router.navigate(['item-details'], {relativeTo: this.route});
-  }
-
-  addItemsToCart(id: number, price: number): void {
-    this.productItemService.setItemPurchaseAmount(this.itemAmountList[id - 1], id, price);
-
-    // Obtains item Price for Calculating Total Purchase Cost
-    this.productItemService.setTotalPurchaseCost(this.itemAmountList[id - 1], price);
   }
 }
